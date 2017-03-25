fb8da1f820a4e0e0733fff31f86035bbaab3d06e
Dimitar <chilly@mail.bg>
2017-03-26 00:36:01 +0200

    2 more :)

---
+++ CircleArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+            Console.WriteLine("{0:f12}", area);
+            Console.Read();
+        }
+    }
+}
