a7ba9af58bd4b8d83b30723ba1267dae6278945f
Dimitar <chilly@mail.bg>
2017-03-26 01:18:24 +0200

    2 more

---
+++ SpecialNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialNumbers
+{
+    class SpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstDigit = 0;
+            int secDigit = 0;
+            int sum = 0;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                firstDigit = i;
+                sum = firstDigit + secDigit;
+                if (i > 10)
+                {
+                    firstDigit = i / 10;
+                    secDigit = i % 10;
+                    sum = firstDigit + secDigit;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                    continue;
+                }
+                Console.WriteLine("{0} -> False", i);
+            }
+        }
+    }
+}
