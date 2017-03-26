0c8e7d6023fd13ae274ebb29a338830b68ac6001
Dimitar <chilly@mail.bg>
2017-03-26 12:32:44 +0300

    finish

---
+++ RefactorVolumeOfPiramid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorVolumeOfPiramid
+{
+    class RefactorVolumeOfPiramid
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Heigth: ");
+            double h = double.Parse(Console.ReadLine());
+            double V = (length + width + h) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", V);
+        }
+    }
+}
---
+++ TriplesOfLatinLatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLatinLatters
+{
+    class TriplesOfLatinLatter
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstL = (char)('a' + i);
+                        char secondL = (char)('a' + j);
+                        char thirdL = (char)('a' + k);
+                        Console.WriteLine("{0}{1}{2}", firstL, secondL, thirdL);
+                    }
+                }
+            }
+        }
+    }
+}
